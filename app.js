426c7b5cdd368e37dc2ee70c7b1255991803aadc
Emmanuel <emmanuelqkorir@gmail.com>
2022-07-19 18:46:27 +0300

    First commit

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
-
-//scope
-let items = ['Buy Food', 'Cook Food', 'Eat Food'];
-let workItems = [];
 
 //setting view engine to ejs
 app.set('view engine', 'ejs');
@@ -17,27 +14,130 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //express to use css, images files. create public directory
 app.use(express.static('public'));
 
+mongoose.connect(
+  'mongodb+srv://emmanuelqkorir:Test123@cluster0.tlang.mongodb.net/todolistDB',
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const itemsSchema = {
+  name: String,
+};
+
+//mongoose model usually capital
+const Item = mongoose.model('Item', itemsSchema);
+
+//crete mongoose document
+
+const item1 = new Item({
+  name: 'Welcome to your todoList!',
+});
+
+const item2 = new Item({
+  name: 'Hit the + button to add a new item.',
+});
+
+const item3 = new Item({
+  name: 'Select checkbox to delete an item.',
+});
+
+//put items in array
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model('list', listSchema);
+
 app.get('/', function (req, res) {
-  let day = date();
-  res.render('list', { listTitle: day, newListItems: items });
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      //insert items in array to mongoose document
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully saved default items to mongoDB.');
+        }
+      });
+      res.redirect('/');
+    } else {
+      res.render('list', {
+        listTitle: 'Today',
+        newListItems: foundItems,
+      });
+    }
+  });
+});
+
+//dynamic express route parameters
+app.get('/:customListName', function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        //create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect('/' + customListName);
+      } else {
+        //show an existing list
+        res.render('list', {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
 });
 
 app.post('/', function (req, res) {
-  let item = req.body.newItem;
-  if (req.body.list === 'Work') {
-    workItems.push(item);
-    res.redirect('/work');
-  } else {
-    items.push(item);
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName,
+  });
+  if (listName === 'Today') {
+    item.save();
     res.redirect('/');
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/' + listName);
+    });
   }
 });
 
-app.get('/work', function (req, res) {
-  res.render('list', {
-    listTitle: 'Work List',
-    newListItems: workItems,
-  });
+app.post('/delete', function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === 'Today') {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log('Successfully deleted checked item.');
+        res.redirect('/');
+      }
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) {
+          res.redirect('/' + listName);
+        }
+      }
+    );
+  }
 });
 
 app.listen(3000, function () {
